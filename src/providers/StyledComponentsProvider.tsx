763579d0100726f89ebcc5c4e99ed44954a5f778
Mirko Basic <bejzik8@gmail.com>
2023-11-17 22:05:48 +0300

    Added light and dark themes. 🍀

--- src/providers/StyledComponentsProvider.tsx
+++ src/providers/StyledComponentsProvider.tsx
@@ -1,9 +1,46 @@
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
 import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import { sky, skyDark } from '@radix-ui/colors'
 
-const theme = {
+const lightTheme = {
     colors: {
-        no1: 'red'
+        no1appBackground: sky.sky1,
+        no2subtleBackground: sky.sky2,
+
+        no3elementBackground: sky.sky3,
+        no4elementBackgroundHovered: sky.sky4,
+        no5elementBackgroundActive: sky.sky5,
+
+        no6bordersNonInteractive: sky.sky6,
+        no7bordersInteractive: sky.sky7,
+        no8bordersInteractiveFocused: sky.sky8,
+
+        no9solidBackground: sky.sky9,
+        no10solidBackgroundHovered: sky.sky10,
+
+        no11textContrastLow: sky.sky11,
+        no12textContrastHigh: sky.sky12
+    }
+}
+
+const darkTheme = {
+    colors: {
+        no1appBackground: skyDark.sky1,
+        no2subtleBackground: skyDark.sky2,
+
+        no3elementBackground: skyDark.sky3,
+        no4elementBackgroundHovered: skyDark.sky4,
+        no5elementBackgroundActive: skyDark.sky5,
+
+        no6bordersNonInteractive: skyDark.sky6,
+        no7bordersInteractive: skyDark.sky7,
+        no8bordersInteractiveFocused: skyDark.sky8,
+
+        no9solidBackground: skyDark.sky9,
+        no10solidBackgroundHovered: skyDark.sky10,
+
+        no11textContrastLow: skyDark.sky11,
+        no12textContrastHigh: skyDark.sky12
     }
 }
 
@@ -28,9 +65,16 @@ const GlobalStyles = createGlobalStyle`
         min-height: 100vh;
         display: flex;
         place-items: center;
+        color: ${({ theme }) => theme.colors.no12textContrastHigh};
+        background-color: ${({ theme }) => theme.colors.no1appBackground};
         margin: 0;
     }
 `
+
+enum Theme {
+    LIGHT,
+    DARK
+}
 
 interface StyledComponentsProviderProps {
     children: ReactNode
@@ -39,8 +83,10 @@ interface StyledComponentsProviderProps {
 export function StyledComponentsProvider({
     children
 }: StyledComponentsProviderProps) {
+    const [theme] = useState(Theme.DARK)
+
     return (
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={theme === Theme.LIGHT ? lightTheme : darkTheme}>
             <GlobalStyles />
             {children}
         </ThemeProvider>
