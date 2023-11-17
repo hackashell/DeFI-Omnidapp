e36038b37d991724d3865dc8946e4c91ecd18fa9
Mirko Basic <bejzik8@gmail.com>
2023-11-17 23:42:20 +0300

    Added styling constants. 🍀

---
+++ src/components/SwapBox/constants/index.ts
@@ -0,0 +1,45 @@
+import { css } from 'styled-components'
+
+// BORDER
+export const BorderStyle = css`
+    border-radius: 12px;
+`
+
+// DIMENSIONS
+export const SWAPBOX_WIDTH = 'min(467px, 100%)'
+
+// MAIN ELEMENTS COLORS
+export const ELEMENTS_BACKGROUND_PRIMARY =
+    'radial-gradient(173.28% 128.28% at 50.64% 0%, rgba(170, 162, 255, 0.06) 0%, rgba(0, 0, 0, 0) 100%), rgba(19, 19, 32, 0.5)'
+export const ELEMENTS_BACKGROUND_SECONDARY =
+    'radial-gradient(160.32% 118.69% at 50.64% 100%, rgba(170, 162, 255, 0.06) 0%, rgba(0, 0, 0, 0) 100%), rgba(19, 19, 32, 0.5)'
+export const ELEMENTS_BORDER_PRIMARY =
+    'linear-gradient(0deg, rgba(68, 65, 99, 0.05), rgba(68, 65, 99, 0.05)) border-box, radial-gradient(40.58% 91.41% at 50.11% -12.63%, rgba(167, 162, 223, 0.1) 0%, rgba(37, 35, 57, 0.02) 100%) border-box'
+export const ELEMENTS_BORDER_SECONDARY =
+    'linear-gradient(0deg, rgba(68, 65, 99, 0.05), rgba(68, 65, 99, 0.05)) border-box, radial-gradient(30.27% 68.18% at 50.11% 100%, rgba(167, 162, 223, 0.1) 0%, rgba(37, 35, 57, 0.02) 100%) border-box'
+
+export const ELEMENTS_SPACING = '6px'
+
+// SWITCH CURRENCIES BUTTON COLORS
+export const SWITCH_CURRENCIES_BUTTON_BACKGROUND_COLOR = '#06060a'
+export const SWITCH_CURRENCIES_BUTTON_BORDER = '1px solid #0c0c14'
+export const SWITCH_CURRENCIES_BUTTON_BOX_SHADOW =
+    '0px 0px 42px rgba(0, 0, 0, 0.42)'
+
+// SWAP INFO
+export const SWAP_PLATFORM_INFO_ITEM_COLOR_DEFAULT =
+    'linear-gradient(180deg, rgba(68, 65, 99, 0.1) -16.91%, rgba(68, 65, 99, 0) 116.18%), linear-gradient(113.18deg, rgba(255, 255, 255, 0.2) -0.1%, rgba(0, 0, 0, 0) 98.9%), #171621'
+export const SWAP_PLATFORM_INFO_ITEM_COLOR_POSITIVE =
+    'linear-gradient(180deg, rgba(15, 152, 106, 0.2) -16.91%, rgba(15, 152, 106, 0) 116.18%), linear-gradient(113.18deg, rgba(255, 255, 255, 0.15) -0.1%, rgba(0, 0, 0, 0) 98.9%), rgba(23, 22, 33, 0.6)'
+export const SWAP_PLATFORM_INFO_ITEM_COLOR_NEGATIVE =
+    'linear-gradient(180deg, rgba(240, 46, 81, 0.2) -16.91%, rgba(240, 46, 81, 0) 116.18%), linear-gradient(113.18deg, rgba(255, 255, 255, 0.15) -0.1%, rgba(0, 0, 0, 0) 98.9%), rgba(23, 22, 33, 0.6)'
+export const SWAP_PLATFORM_INFO_ITEM_BORDER_DEFAULT =
+    'linear-gradient(0deg, rgba(68, 65, 99, 0.3), rgba(68, 65, 99, 0.3)) border-box, radial-gradient(102.67% 104.54% at 50.11% -12.63%, rgba(68, 65, 99, 0.5) 0%, rgba(37, 35, 57, 0.05) 100%) border-box'
+export const SWAP_PLATFORM_INFO_ITEM_BORDER_POSITIVE =
+    'linear-gradient(0deg, rgba(14, 159, 110, 0.3), rgba(14, 159, 110, 0.3)) border-box, radial-gradient(102.67% 104.54% at 50.11% -12.63%, rgba(14, 159, 110, 0.5) 0%, rgba(37, 35, 57, 0.05) 100%) border-box'
+export const SWAP_PLATFORM_INFO_ITEM_BORDER_NEGATIVE =
+    'linear-gradient(0deg, rgba(240, 46, 81, 0.3), rgba(240, 46, 81, 0.3)) border-box, radial-gradient(102.67% 104.54% at 50.11% -12.63%, rgba(14, 159, 110, 0.5) 0%, rgba(37, 35, 57, 0.05) 100%) border-box'
+
+// TOKEN PICKER COLORS
+export const TOKEN_PICKER_CLOSE_BUTTON_BACKGROUND_COLOR =
+    'rgba(60, 56, 100, 0.1)'
