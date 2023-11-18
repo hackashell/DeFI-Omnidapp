b2f05ac690b0818778c283d7e22568c6728037cc
Mirko Basic <bejzik8@gmail.com>
2023-11-19 02:02:17 +0300

    Updated members section. 🍀

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,41 @@
 import styled from 'styled-components'
 
+type Member = {
+    firstName: string
+    lastName: string
+    photoUrl: string
+}
+
+const members: Member[] = [
+    {
+        firstName: 'Mirko',
+        lastName: 'Basic',
+        photoUrl: 'https://avatars.githubusercontent.com/u/24759563?v=4'
+    },
+    {
+        firstName: 'Petar',
+        lastName: 'Popovic',
+        photoUrl: 'https://avatars.githubusercontent.com/u/9700675?v=4'
+    },
+    {
+        firstName: 'Viraz',
+        lastName: 'Malhotra',
+        photoUrl: 'https://avatars.githubusercontent.com/u/26670962?v=4'
+    }
+]
+
 export const Footer = () => (
     <Container>
-        <ProfilePhoto src='https://avatars.githubusercontent.com/u/24759563?v=4' />
-        <ProfilePhoto src='https://avatars.githubusercontent.com/u/9700675?v=4' />
-        <ProfilePhoto src='https://avatars.githubusercontent.com/u/26670962?v=4' />
+        {members.map(member => (
+            <Card>
+                <ProfilePhoto
+                    key={`${member.firstName}-${member.lastName}`}
+                    src={member.photoUrl}
+                />
+                <Paragraph>{member.firstName}</Paragraph>
+                <Paragraph>{member.lastName}</Paragraph>
+            </Card>
+        ))}
     </Container>
 )
 
@@ -12,8 +43,24 @@ const Container = styled.footer`
     height: 200px;
     background: linear-gradient(
         ${({ theme }) => theme.colors.no2subtleBackground},
+        ${({ theme }) => theme.colors.no1appBackground}
+    );
+`
+
+const Card = styled.div`
+    display: inline-block;
+    padding: 1rem;
+    background: linear-gradient(
+        ${({ theme }) => theme.colors.no2subtleBackground},
         ${({ theme }) => theme.colors.no3elementBackground}
     );
+    border-radius: 0.5rem;
+`
+
+const Paragraph = styled.p`
+    line-height: 1;
+    font-size: 1rem;
+    text-align: center;
 `
 
 const ProfilePhoto = styled.img`
