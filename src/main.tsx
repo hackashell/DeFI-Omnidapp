21bafaf3206acc5b58a932ba44c33d7361139b31
Mirko Basic <bejzik8@gmail.com>
2023-11-18 16:33:02 +0300

    Created ReactQueryProvider component. 🍀

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-import { StyledComponentsProvider, WagmiProvider } from '@/providers'
+import {
+    StyledComponentsProvider,
+    ReactQueryProvider,
+    WagmiProvider
+} from '@/providers'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <WagmiProvider>
-            <StyledComponentsProvider>
-                <App />
-            </StyledComponentsProvider>
-        </WagmiProvider>
+        <ReactQueryProvider>
+            <WagmiProvider>
+                <StyledComponentsProvider>
+                    <App />
+                </StyledComponentsProvider>
+            </WagmiProvider>
+        </ReactQueryProvider>
     </React.StrictMode>
 )
