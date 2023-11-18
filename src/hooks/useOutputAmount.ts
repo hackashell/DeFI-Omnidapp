988a79785b05ffc5ea2fb0b3f5afcc55c0c5990c
Mirko Basic <bejzik8@gmail.com>
2023-11-18 21:49:48 +0300

    Updated useOutputAmount hook. 🍀

--- src/hooks/useOutputAmount.ts
+++ src/hooks/useOutputAmount.ts
@@ -1,25 +1,16 @@
 import { useMutation } from '@tanstack/react-query'
 import axios from 'axios'
 
-const oneInchApiKey = import.meta.env.VITE_ONE_INCH_API_KEY
-
 export const useOutputAmount = () => {
     const getOutputAmount = (variables: {
         from: string
         to: string
         amount: string
     }) => {
-        const { from, to, amount } = variables
-        return axios.get('https://api.1inch.dev/swap/v5.2/1/quote', {
-            headers: {
-                Authorization: `Bearer ${oneInchApiKey}`
-            },
-            params: {
-                src: from,
-                dst: to,
-                amount: amount
-            }
-        })
+        return axios.post(
+            'http://localhost:3000/one-inch/find-best-quote',
+            variables
+        )
     }
 
     return useMutation({
