f6485ca2a5547a3af4bbc627c341b1be5a141a3c
Mirko Basic <bejzik8@gmail.com>
2023-11-19 01:15:22 +0300

    Created getTokenList function. 🍀

--- src/controllers/oneinch.ts
+++ src/controllers/oneinch.ts
@@ -1,10 +1,23 @@
 import { RequestHandler } from 'express'
 import axios from 'axios'
 
+export const getTokenList: RequestHandler = async (_, res, next) => {
+    try {
+        const { data } = await axios.get('https://api.1inch.dev/token/v1.2/1', {
+            headers: {
+                Authorization: `Bearer ${process.env.VITE_ONE_INCH_API_KEY}`
+            }
+        })
+
+        res.status(200).json({ response: data })
+    } catch (err) {
+        next(err)
+    }
+}
+
 export const getAmountTo: RequestHandler = async (req, res, next) => {
     const { chainId, from, to, amount } = req.body
 
-    console.log(req.body.chainId)
     try {
         const { data } = await axios.get(
             `https://api.1inch.dev/swap/v5.2/${chainId}/quote`,
