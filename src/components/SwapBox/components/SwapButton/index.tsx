f1d810e98ca3848abdb0d2cab724a9b338451ec0
Mirko Basic <bejzik8@gmail.com>
2023-11-18 19:31:06 +0300

    Updated SwapButton component. 🍀

--- src/components/SwapBox/components/SwapButton/index.tsx
+++ src/components/SwapBox/components/SwapButton/index.tsx
@@ -10,7 +10,7 @@ export const SwapButton = ({ chain }: SwapButtonProps) => {
     const getButtonLabel = () => {
         if (!chain) return 'Please Connect a Wallet'
 
-        return 'Approve'
+        return 'APPROVE'
     }
     return <StyledButton disabled={!chain}>{getButtonLabel()}</StyledButton>
 }
@@ -24,9 +24,10 @@ const StyledButton = styled.button`
     gap: 10px;
     border: none;
     border-radius: 12px;
+    font-weight: 700;
     box-shadow: 0px 0px 42px rgba(129, 62, 127, 0.32);
-    color: ${({ theme }) => theme.colors.no12textContrastHigh};
-    background: ${({ theme }) => theme.colors.no8bordersInteractiveFocused};
+    color: ${({ theme }) => theme.colors.no1appBackground};
+    background: linear-gradient(300deg, #93f5ec 20%, #a77bf3 70%);
     cursor: pointer;
 
     &:hover {
