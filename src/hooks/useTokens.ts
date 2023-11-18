8cc3a72aa59684313690b7b5f7efc09caa99a704
Mirko Basic <bejzik8@gmail.com>
2023-11-18 16:32:37 +0300

    Created useTokens fetching hook. 🍀

---
+++ src/hooks/useTokens.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import { TokenInfo, TokenList } from '@uniswap/token-lists'
+import { useQuery } from '@tanstack/react-query'
+
+const UNISWAP_DEFAULT = 'https://gateway.ipfs.io/ipns/tokens.uniswap.org'
+
+export const useTokens = () => {
+    const getTokens = async () => {
+        let tokens: TokenInfo[] = []
+
+        try {
+            const res = await axios.get<TokenList>(UNISWAP_DEFAULT)
+
+            tokens = res.data.tokens
+        } catch (error) {
+            console.log(error)
+        }
+
+        return tokens
+    }
+
+    return useQuery({
+        queryKey: ['todos'],
+        queryFn: getTokens,
+        refetchOnWindowFocus: false
+    })
+}
